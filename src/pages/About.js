5024e7dd564f267221f0b3a0c27cc83a7cb8e0ec
Tracey <traceydu0122@gmail.com>
2022-01-17 00:17:08 -0800

    Update About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,11 @@
 import '../App.css';
-import { Row, Col, Container } from "react-grid-system";
+import { Row, Col } from "react-grid-system";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import BigPhoto from "../assets/images/about/about.png";
+import SmallPhoto1 from "../assets/images/about/about1.png";
+import SmallPhoto2 from "../assets/images/about/about2.png";
+import SmallPhoto3 from "../assets/images/about/about3.png";
 
 function About() {
   return (
@@ -9,29 +13,30 @@ function About() {
       <Navbar/>
         <div className="about">
           <div className="about-section1">
-            <Row>
+            <Row justify="center" align="center">
               <Col md={6} xs={0}>
+                <img src={BigPhoto} className="big-photo" />
               </Col>
-              <Col md={6} xs={{span:12, order: 1}}>
+              <Col md={6} xs={12}>
                 <h1>About</h1>
-                <h2>Centered around Designers</h2>
+                <h2>Uplifting Student Designers</h2>
                 <p>
-                  Stride unites aspiring designers with companies seeking designers. 
-                  Students will have the chance to share their work and experience 
-                  with design-minded company representatives. Although having a 
-                  portfolio isn't a requirement, we strongly encourage students 
-                  interested in attending to create one to demonstrate your design 
-                  skills put into practice.
+                  Stride is a uniquely design-forward career fair aiming to connect 
+                  aspiring designers with company representatives. Students are encouraged 
+                  to showcase their work and experience, so while having a portfolio isn’t 
+                  a requirement, we suggest that attendees create one to demonstrate your 
+                  design skills put into practice.
                 </p>
-                <h2>An Environment that Promotes Conversation</h2>
+                <h2>Encouraging Conversation</h2>
                 <p>
-                  At Stride, we find that the scaled down setting allows for employers 
-                  and students to engage in longer, more fruitful conversations. 
-                  Instead of just handing off a resume, it's common and highly 
-                  encouraged for 5-10 minute mini portfolio reviews to take place.
+                  At Stride, we want to facilitate memorable conversations—rather than 
+                  handing off a resume, Stride is oriented around 5-10 minute mini portfolio 
+                  reviews. Especially in a virtual setting, the goal of Stride is to promote 
+                  meaningful engagement for both students and employers.
                 </p>
               </Col>
               <Col md={0} xs={12}>
+                <img src={BigPhoto} className="big-photo-mobile" />
               </Col>
             </Row>
           </div>
@@ -40,40 +45,48 @@ function About() {
             <div className="qa">
               <p><b>Q: When is Stride?</b></p>
               <p>
-                <b>A:</b> Stride takes place on Tuesday, January 14th, 2020 from 11AM to 4PM.
+                <b>A:</b> Stride takes place on Wednesday, February 5th, 2022. It will be 
+                from 9am-2pm with a break from 11am-12pm. <b>This year, Stride will be a completely 
+                virtual event held over Zoom.</b>
               </p>
             </div>
             <div className="qa">
-              <p><b>Q: What time can I arrive</b></p>
+              <p><b>Q: What will Stride look like on Zoom?</b></p>
               <p>
-                <b>A:</b> Doors open at 11AM sharp. There may be some average lines, so 
-                we recommend arriving a little earlier. Note that doors close at 3:45PM.
+                <b>A:</b> Students will be sorted into breakout rooms with a recruiter. 
+                For the most up-to-date information, check our social media @ucsddesignco
               </p>
             </div>
             <div className="qa">
               <p><b>Q: Who can attend Stride?</b></p>
               <p>
-                <b>A:</b> Any UCSD students or alum with a valid UCSD student ID.
+                <b>A:</b> All UCSD students and alumni are welcome! There is a limited 
+                number of recruiters and time with each one (~15 min), so students will 
+                be assigned to breakout rooms on a first come first serve basis. Be sure 
+                to sign up and secure your spot through our&nbsp;
+                <a href="" target="_blank">registration form</a> 
+                &nbsp;by Sunday, 1/30.
               </p>
             </div>
             <div className="qa">
               <p><b>Q: How should I dress for Stride?</b></p>
               <p>
                 <b>A:</b> While there is no dress code for Stride, we do recommend making 
-                sure you look “presentable”. Your interpretation of "presentable" 
-                is up to you--some people dress in business attire, while others 
-                dress as they normally do. Maybe skip the shirt with a stain?
+                sure you look “presentable” (at least in your Zoom window!). What "presentable" 
+                looks like is up to you—some people dress in business attire, while others dress 
+                as they normally do.
               </p>
             </div>
             <div className="qa">
               <p><b>Q: How can I prepare for Stride?</b></p>
               <p>
-                <b>A:</b> There are lots of ways to prepare for Stride! From preparing a 
-                portfolio to revamping your resume, we've put together a slide 
-                deck for you with advice on how to ready yourself for Stride.&nbsp; 
+                <b>A:</b> From preparing a portfolio to revamping your resume, we've put together a&nbsp; 
                 <a href="https://docs.google.com/presentation/d/159kEktP6bRSRWa0fG8Jxyl-CeiORcMJOUuwuvou8oZY/edit?usp=sharing">
-                  Click here to check it out!
+                  slide deck
                 </a>
+                &nbsp;for you with advice on how to prepare yourself for Stride. Due to the virtual nature 
+                of Stride 2022, we suggest having any materials ready to go on your computer, along 
+                with a stable Internet connection, mic, and webcam.
               </p>
             </div>
             <div className="qa">
@@ -87,12 +100,15 @@ function About() {
           <div className="about-section3">
             <Row>
               <Col md={4} xs={12}>
+                  <img src={SmallPhoto1} className="small-photo" />
                   <h2>Arrive between<br />11AM - 3:45PM</h2>
               </Col>
               <Col md={4} xs={12}>
+                <img src={SmallPhoto2} className="small-photo" />
                 <h2>Review portfolio with a recruiter</h2>
               </Col>
               <Col md={4} xs={12}>
+                <img src={SmallPhoto3} className="small-photo" />
                 <h2>Network with companies</h2>
               </Col>
             </Row>
